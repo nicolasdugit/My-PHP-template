2e403c2c7c3e018ac50689bcd098527e147fbd10
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-09-27 16:35:18 +0700

    first commit

---
+++ model/Manager.php
@@ -0,0 +1,34 @@
+<?php
+namespace MonNameSpace\Model;
+
+require_once 'db_config.php'; //db Confing File
+/**
+ * 
+ */
+class Manager {
+    private $driver;
+    private $host;
+    private $user;
+    private $pass;
+    private $database;
+    private $charset;
+    /**
+     * [__construct description]
+     */
+    public function __construct() {
+        //Vacia variabes constantes
+        $this->driver   = DB_DRIVER;
+        $this->host     = HOST;
+        $this->user     = DB_USER;
+        $this->pass     = DB_PASSWORD;
+        $this->database = DB_NAME;
+        $this->charset  = CHARSET;
+    }
+    /**
+     * [DBConnect description]
+     */
+    protected function DBConnect() {
+        $db = new \PDO($this->driver.":host=".$this->host.";dbname=".$this->database.";charset=".$this->charset, $this->user, $this->pass);
+        return ($db);
+    }
+}
